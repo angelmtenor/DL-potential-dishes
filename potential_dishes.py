2bf04397fdb07ebd1d79227b7fff7fbabcb27cef
angelmtenor <angelmtenor@gmail.com>
2021-05-08 17:57:49 +0200

    fix: Avoid issues new versions of tf/keras

--- potential_dishes.py
+++ potential_dishes.py
@@ -6,7 +6,6 @@ Goal: Get samples that could potentially be considered as a combination of Sandw
 
 """
 
-
 import os
 from time import time
 
@@ -27,7 +26,9 @@ from keras.models import Sequential, Model
 from keras.layers import Dropout, Flatten, Dense, MaxPooling2D, Conv2D, InputLayer, Activation
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-import helper  # custom library for this assigment
+import helper_ml
+
+sns.set_palette("Reds")
 
 # Files and Folders. Dataset-dependent
 SOURCE_FILE = 'http://research.us-east-1.s3.amazonaws.com/public/sushi_or_sandwich_photos.zip'
@@ -57,12 +58,11 @@ def setup():
     Then split the data into training and validation sets and save them in separated folders
     """
 
-    print("\n setup .... \n")
-    helper.info_gpu()
+    print("\nsetup .... \n")
+    helper_ml.info_gpu()
     sns.set_palette("Reds")
     # set reproducible results from run to run with Keras
-    helper.reproducible(seed=SEED)
-
+    helper_ml.reproducible(seed=SEED)
     """ 1. Donwload and extract the pictures """
     # Download the pictures
     if not os.path.isfile(DATA_FILE):
@@ -82,10 +82,8 @@ def setup():
     print("\nPictures:")
     for c in CLASSES:
         path = os.path.join(DATA_DIR, c)
-        print("{}   \t{}".format(
-            c,
-            len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])))
-
+        print("{}   \t{}".format(c,
+                                 len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])))
     """ Split the data into training and validation sets (not enough data for 3 partitions) """
 
     # remove existing sets
@@ -105,8 +103,7 @@ def setup():
         train_val_split = int(len(files) * (VALIDATION_SIZE))
         for i, ix in enumerate(indices):
             src = files[ix]
-            dest = '{}/{}/{}'.format(VALIDATION_DIR if i < train_val_split else TRAIN_DIR, c,
-                                     files[ix].split('/')[-1])
+            dest = '{}/{}/{}'.format(VALIDATION_DIR if i < train_val_split else TRAIN_DIR, c, files[ix].split('/')[-1])
             shutil.copyfile(src, dest)
 
     # Print the size of each set
@@ -114,9 +111,7 @@ def setup():
     for d in (TRAIN_DIR, VALIDATION_DIR):
         for c in CLASSES:
             path = os.path.join(d, c)
-            print("{} {}  {}".format(
-                d, c,
-                len([n for n in os.listdir(path) if os.path.isfile(os.path.join(path, n))])))
+            print("{} {}  {}".format(d, c, len([n for n in os.listdir(path) if os.path.isfile(os.path.join(path, n))])))
 
     print("\nsetup .... OK")
 
@@ -124,8 +119,9 @@ def setup():
 def get_bottleneck(train_datagen, val_datagen):
     """ Use a pretrained convolutional model to extract the bottleneck features """
 
-    model_bottleneck = keras.applications.MobileNet(
-        weights='imagenet', include_top=False, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3))
+    model_bottleneck = keras.applications.MobileNet(weights='imagenet',
+                                                    include_top=False,
+                                                    input_shape=(IMG_HEIGHT, IMG_WIDTH, 3))
 
     for layer in model_bottleneck.layers:
         layer.trainable = False
@@ -152,10 +148,8 @@ def get_bottleneck(train_datagen, val_datagen):
 
     print('\n Extracting bottleneck features:')
 
-    train_bottleneck = model_bottleneck.predict_generator(
-        train_bottleneck_generator, verbose=1)
-    val_bottleneck = model_bottleneck.predict_generator(
-        val_bottleneck_generator, verbose=1)
+    train_bottleneck = model_bottleneck.predict(train_bottleneck_generator, verbose=1)
+    val_bottleneck = model_bottleneck.predict(val_bottleneck_generator, verbose=1)
     train_labels = train_bottleneck_generator.classes
     val_labels = val_bottleneck_generator.classes
 
@@ -166,8 +160,7 @@ def build_top_nn(input_shape, summary=False):
     """" Return the custom fully connected classifier """
 
     w = keras.initializers.TruncatedNormal(mean=0.0, stddev=0.0001, seed=None)
-    opt = keras.optimizers.Adamax(
-        lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
+    opt = keras.optimizers.Adamax(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0)
 
     model_top = Sequential()
     model_top.add(Flatten(input_shape=input_shape))
@@ -181,8 +174,7 @@ def build_top_nn(input_shape, summary=False):
         print("Top classifier:")
         model_top.summary()
 
-    model_top.compile(
-        optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
+    model_top.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
 
     return model_top
 
@@ -190,34 +182,31 @@ def build_top_nn(input_shape, summary=False):
 def train_nn(model_top, train_bottleneck, val_bottleneck, train_labels, val_labels, show_plots=False):
     """ Train the custom classifier (with the input bottleneck features) """
 
-    checkpoint = ModelCheckpoint(
-        "checkpoint-top.h5",
-        monitor='val_acc',
-        verbose=0,
-        save_best_only=True,
-        save_weights_only=False,
-        mode='auto',
-        period=1)
+    checkpoint = ModelCheckpoint("checkpoint-top.h5",
+                                 monitor='val_accuracy',
+                                 verbose=0,
+                                 save_best_only=True,
+                                 save_weights_only=False,
+                                 mode='auto',
+                                 save_freq=1)
 
-    early = EarlyStopping(
-        monitor='val_acc', min_delta=0, patience=50, verbose=0, mode='auto')
+    early = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=50, verbose=0, mode='auto')
 
     print('\nTraining neural network....')
     t0 = time()
 
-    history = model_top.fit(
-        train_bottleneck,
-        train_labels,
-        epochs=500,
-        batch_size=BATCH_SIZE,
-        verbose=0,
-        validation_data=(val_bottleneck, val_labels),
-        callbacks=[checkpoint, early])
+    history = model_top.fit(train_bottleneck,
+                            train_labels,
+                            epochs=500,
+                            batch_size=BATCH_SIZE,
+                            verbose=0,
+                            validation_data=(val_bottleneck, val_labels),
+                            callbacks=[checkpoint, early])
 
     print("time: \t {:.1f} s".format(time() - t0))
 
     if show_plots:
-        helper.show_training(history)
+        helper_ml.show_training(history)
 
     # restore best model found (callback-checkpoint)
     model_top = None
@@ -232,13 +221,9 @@ def build_full_model(model_bottleneck, model_top):
     """ Build the full model (pretrained bottleneck + custom classifier) """
 
     # stack Layers using Keras's fucntional approach:
-    full_model = Model(inputs=model_bottleneck.input,
-                       outputs=model_top(model_bottleneck.output))
+    full_model = Model(inputs=model_bottleneck.input, outputs=model_top(model_bottleneck.output))
 
-    full_model.compile(
-        optimizer='adam',
-        loss='binary_crossentropy',
-        metrics=['accuracy'])
+    full_model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     return full_model
 
@@ -281,12 +266,8 @@ def predict_and_save_potential_dishes(full_model, val_datagen):
     plt.close()
 
 
-if __name__ == '__main__':
-
-    # 1. Download, extract, slit the pictures (train, validation) and save them
-    setup()
-
-    # 2. Create image generators with data augmentation
+def creteate_image_generators():
+    """ Create image generators with data augmentation """
     train_datagen = ImageDataGenerator(
         rescale=1. / 255,
         shear_range=0.4,  # high change of persperctive in this pictures
@@ -294,15 +275,67 @@ if __name__ == '__main__':
         horizontal_flip=True)
     val_datagen = ImageDataGenerator(rescale=1 / 255)
 
+    return train_datagen, val_datagen
+
+
+""" AUXILIARY FOR DEV / EDA """
+
+
+def load_samples(path, size):
+    """ load and return an array of images """
+    imagesList = os.listdir(path)
+    samples = []
+    for image in imagesList[:size]:
+        img = Image.open(os.path.join(path, image))
+        samples.append(img)
+    return samples
+
+
+def plot_samples(size=18):
+    """ Plot some pictures of dishes of the dataset """
+    for c in ('sandwich', 'sushi'):
+        path = os.path.join(DATA_DIR, c)
+        imgs = load_samples(path, size)
+
+        plt.figure(figsize=(16, 8))
+        gcf().suptitle(c + " samples", fontsize=18)
+
+        for i, img in enumerate(imgs):
+            # you can show every image
+            plt.subplot(3, 6, i + 1)
+            plt.imshow(img)
+            plt.axis('off')
+
+    # Print the number of pictures
+    print("pictures:")
+    for c in ('sandwich', 'sushi'):
+        path = os.path.join(DATA_DIR, c)
+        print("{}   \t{}".format(c,
+                                 len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name))])))
+
+
+"""    MAIN   """
+
+if __name__ == '__main__':
+
+    # 1. Download, extract & split the pictures (train, validation)
+    setup()
+
+    # 2. Create image generators with data augmentation
+    train_datagen, val_datagen = creteate_image_generators()
+
     # 3. Use a pretrained convolutional model to extract the bottleneck features
     model_bottleneck, train_bottleneck, val_bottleneck, train_labels, val_labels = get_bottleneck(
         train_datagen, val_datagen)
 
     # 4. Build and train the top classifier
-    model_top = build_top_nn(
-        input_shape=train_bottleneck.shape[1:], summary=True)
-    model_top = train_nn(model_top, train_bottleneck,
-                         val_bottleneck, train_labels, val_labels, show_plots=SHOW_TRAINING_PLOT)
+    model_top = build_top_nn(input_shape=train_bottleneck.shape[1:], summary=True)
+    model_top = train_nn(model_top,
+                         train_bottleneck,
+                         val_bottleneck,
+                         train_labels,
+                         val_labels,
+                         show_plots=SHOW_TRAINING_PLOT)
 
     # 5. Build the complete trained model, make predictions, and save potential dishes
     full_model = build_full_model(model_bottleneck, model_top)
